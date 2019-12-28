bb131506e5938f3da376c73c2a1a5c9dc6c6d737
stefanji <408776303@qq.com>
2019-12-28 09:02:20 +0800

    update emoji

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ async function run() {
     if (config.input_files) {
       const files = paths(config.input_files)
       if (files.length == 0) {
-        console.warn(`⚠️ ${config.input_files} not include valid file.`)
+        console.warn(`🤔 ${config.input_files} not include valid file.`)
       }
       files.forEach(async path => {
         await upload(gh, rel.upload_url, path);
