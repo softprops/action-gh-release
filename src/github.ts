d9a17b2b70a11ff00e4a7d0be3ca04e74d66de24
firelight flagboy <firelight.flagboy@gmail.com>
2023-06-29 14:01:41 +0200

    Correct `_release` undefined when creating a new release

--- src/github.ts
+++ src/github.ts
@@ -218,7 +218,18 @@ export const release = async (
         })
       ).data;
     }
-    let existingRelease = _release!;
+    if (_release === null || _release === undefined) {
+      return await createRelease(
+        tag,
+        config,
+        releaser,
+        owner,
+        repo,
+        maxRetries
+      );
+    }
+
+    let existingRelease: Release = _release!;
     console.log(
       `Found release ${existingRelease.name} (with id=${existingRelease.id})`
     );
@@ -277,49 +288,65 @@ export const release = async (
     return release.data;
   } catch (error) {
     if (error.status === 404) {
-      const tag_name = tag;
-      const name = config.input_name || tag;
-      const body = releaseBody(config);
-      const draft = config.input_draft;
-      const prerelease = config.input_prerelease;
-      const target_commitish = config.input_target_commitish;
-      let commitMessage: string = "";
-      if (target_commitish) {
-        commitMessage = ` using commit "${target_commitish}"`;
-      }
-      console.log(
-        `👩‍🏭 Creating new GitHub release for tag ${tag_name}${commitMessage}...`
+      return await createRelease(
+        tag,
+        config,
+        releaser,
+        owner,
+        repo,
+        maxRetries
       );
-      try {
-        let release = await releaser.createRelease({
-          owner,
-          repo,
-          tag_name,
-          name,
-          body,
-          draft,
-          prerelease,
-          target_commitish,
-          discussion_category_name,
-          generate_release_notes,
-        });
-        return release.data;
-      } catch (error) {
-        // presume a race with competing metrix runs
-        console.log(
-          `⚠️ GitHub release failed with status: ${
-            error.status
-          }\n${JSON.stringify(error.response.data.errors)}\nretrying... (${
-            maxRetries - 1
-          } retries remaining)`
-        );
-        return release(config, releaser, maxRetries - 1);
-      }
     } else {
-      console.log(
+      console.trace(
         `⚠️ Unexpected error fetching GitHub release for tag ${config.github_ref}: ${error}`
       );
       throw error;
     }
   }
 };
+
+async function createRelease(
+  tag: string,
+  config: Config,
+  releaser: Releaser,
+  owner: string,
+  repo: string,
+  maxRetries: number
+) {
+  const tag_name = tag;
+  const name = config.input_name || tag;
+  const body = releaseBody(config);
+  const draft = config.input_draft;
+  const prerelease = config.input_prerelease;
+  const target_commitish = config.input_target_commitish;
+  let commitMessage: string = "";
+  if (target_commitish) {
+    commitMessage = ` using commit "${target_commitish}"`;
+  }
+  console.log(
+    `👩‍🏭 Creating new GitHub release for tag ${tag_name}${commitMessage}...`
+  );
+  try {
+    let release = await releaser.createRelease({
+      owner,
+      repo,
+      tag_name,
+      name,
+      body,
+      draft,
+      prerelease,
+      target_commitish,
+      discussion_category_name: config.input_discussion_category_name,
+      generate_release_notes: config.input_generate_release_notes,
+    });
+    return release.data;
+  } catch (error) {
+    // presume a race with competing matrix runs
+    console.log(
+      `⚠️ GitHub release failed with status: ${error.status}\n${JSON.stringify(
+        error.response.data.errors
+      )}\nretrying... (${maxRetries - 1} retries remaining)`
+    );
+    return release(config, releaser, maxRetries - 1);
+  }
+}
