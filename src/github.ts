d67838e5786c2eec2084db89e3a60f3587b9b4d9
WANG Xuerui <git@xen0n.name>
2024-12-23 15:15:11 +0800

    style: format with prettier
    Signed-off-by: WANG Xuerui <git@xen0n.name>

--- src/github.ts
+++ src/github.ts
@@ -188,7 +188,7 @@ export const upload = async (
         "content-type": mime,
         authorization: `token ${config.github_token}`,
       },
-      data: fh.readableWebStream({type: "bytes"}),
+      data: fh.readableWebStream({ type: "bytes" }),
     });
     const json = resp.data;
     if (resp.status !== 201) {
