bd82c38418055a59d4806d77f9bc2bb3117d9c80
Taj <tajulislamj200@gmail.com>
2025-08-24 02:54:48 +0530

    feat: Separate interfaces into dedicated files and folder

--- src/util.ts
+++ src/util.ts
@@ -1,28 +1,7 @@
 import * as glob from 'glob';
 import { statSync, readFileSync } from 'fs';
 
-export interface Config {
-  github_token: string;
-  github_ref: string;
-  github_repository: string;
-  // user provided
-  input_name?: string;
-  input_tag_name?: string;
-  input_repository?: string;
-  input_body?: string;
-  input_body_path?: string;
-  input_files?: string[];
-  input_overwrite_files?: boolean;
-  input_draft?: boolean;
-  input_preserve_order?: boolean;
-  input_prerelease?: boolean;
-  input_fail_on_unmatched_files?: boolean;
-  input_target_commitish?: string;
-  input_discussion_category_name?: string;
-  input_generate_release_notes?: boolean;
-  input_append_body?: boolean;
-  input_make_latest: 'true' | 'false' | 'legacy' | undefined;
-}
+import { Config } from './interfaces/configInterface';
 
 export const uploadUrl = (url: string): string => {
   const templateMarkerPos = url.indexOf('{');
